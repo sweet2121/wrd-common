c1584be35c24cbd4bb3b11a72b9293fe44e920a9
wangruidan <2415726082@qq.com>
2020-03-26 19:24:54 +0800

    提交StringUtil

--- src/test/java/com/wrd/common/utils/StringUtilTest.java
+++ src/test/java/com/wrd/common/utils/StringUtilTest.java
@@ -29,7 +29,7 @@ public class StringUtilTest {
 
 	@Test
 	public void testGenerateChineseName() {
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 100000; i++) {
 			String name = StringUtil.generateChineseName();
 			System.out.println(name);
 		}
